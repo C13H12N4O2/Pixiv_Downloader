fe2516970e494b6d546ae66063e023d009b064e5
C13H12N4O2 <c13h12n4o2@C13H12N4O2s-MacBook-Pro.local>
2021-12-17 19:52:36 +0900

    유저명 추가 유저설명은 Pixiv_Crawler API에서 유저설명 데이터를 가져오는 기능을 지원하지 않아서 추가 불가능 - #6

--- Pixiv_Downloader/Widgets/Main.py
+++ Pixiv_Downloader/Widgets/Main.py
@@ -3,7 +3,7 @@ from Libraries.Pixiv_Crawler import Pixiv
 from Widgets.Labels.UserIconLabel import UserIconLabel
 from Widgets.ProgressBar.ProgressBar import ProgressBar
 from PyQt6.QtCore import Qt, QRunnable, QThreadPool
-from PyQt6.QtWidgets import QPushButton, QLineEdit, QHBoxLayout, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QPushButton, QLabel, QLineEdit, QHBoxLayout, QVBoxLayout, QWidget
 
 
 class Main(QWidget):
@@ -15,12 +15,14 @@ class Main(QWidget):
     
   def initUI(self):
     self.headerHLayout()
+    self.infoVLayout()
     self.functionHLayout()
     self.setButton()
     self.progressBarHLayout()
   
     layout = QVBoxLayout()
     layout.addLayout(self.headerHLayout)
+    layout.addLayout(self.infoVLayout)
     layout.addLayout(self.functionHLayout)
     layout.addLayout(self.progressBarHLayout)
     
@@ -39,21 +41,23 @@ class Main(QWidget):
     if self.progressBar.value() > 0:
       self.progressBar.setValue(0)
     
-    self.changeIconImg(uid)
+    self.changeUserInfo(uid)
     
     pool = QThreadPool.globalInstance()
     dl = Download(uid, self.pixiv, self.progressBar.increase, self.progressBar.setMaximum)
     pool.start(dl)
     
     
-  def changeIconImg(self, uid):
+  def changeUserInfo(self, uid):
     res = self.pixiv.user_detail(uid, is_pc=True)
+    name = res['body']['name']
     url = res['body']['imageBig']
     
     img = self.pixiv.img_data(url)
     if self.isSame(img):
       return
-      
+    
+    self.userNameLabel.setText(name)
     self.userIconLabel.img = img
     self.userIconLabel.isDefault = False
     self.userIconLabel.setLabel()
@@ -74,6 +78,14 @@ class Main(QWidget):
     self.headerHLayout.addStretch(10)
     self.headerHLayout.addWidget(self.userIconLabel)
     self.headerHLayout.addStretch(10)
+    
+    
+  def infoVLayout(self):
+    self.userNameLabel = QLabel("User Name")
+    self.userNameLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    
+    self.infoVLayout = QVBoxLayout()
+    self.infoVLayout.addWidget(self.userNameLabel)
     
     
   def functionHLayout(self):
